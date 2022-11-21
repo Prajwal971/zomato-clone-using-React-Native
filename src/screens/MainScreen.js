8dcf0ffc6c5f41699460c7ba3184ec70d9d1fb5e
prajwal971 <prajwalkumar971@gmail.com>
2022-11-21 14:18:03 +0400

    bottom nav images added.

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { responsiveHeight, responsiveScreenHeight, responsiveWidth } from 'react-native-responsive-dimensions'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const MainScreen = () => {
-  return (
-    <View>
-      <Text style={{color:'black'}}>MainScreen</Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={styles.bottomNavigationView}>
+                <TouchableOpacity style={styles.tab}>
+
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default MainScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    bottomNavigationView: {
+        height: responsiveHeight(8),
+        width: responsiveWidth(100),
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    tab: {
+        width: '30%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
