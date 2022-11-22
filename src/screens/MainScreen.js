322429982ad80fedfd52849dd7aac506c578c83a
prajwal971 <prajwalkumar971@gmail.com>
2022-11-22 16:38:26 +0400

    searchBar added.

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,25 +1,29 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
+import Tab1 from '../tabs/Tab1'
+import Tab2 from '../tabs/Tab2'
+import Tab3 from '../tabs/Tab3'
 
 const MainScreen = () => {
     const [selectedTab, setSelectedTab] = useState(0)
     return (
         <View style={styles.container}>
+            {selectedTab == 0 ? <Tab1 /> : selectedTab == 1 ? <Tab2 /> : <Tab3 />}
             <View style={styles.bottomNavigationView}>
-                <TouchableOpacity style={[styles.tab,{borderTopWidth:1,borderTopColor:selectedTab == 0 ? 'red' : 'white'}]} onPress={() => { setSelectedTab(0) }}>
+                <TouchableOpacity style={[styles.tab, { borderTopWidth: 1, borderTopColor: selectedTab == 0 ? 'red' : 'white' }]} onPress={() => { setSelectedTab(0) }}>
                     <Image source={require('../images/delivery.png')} style={[styles.tabIcon, { tintColor: selectedTab == 0 ? 'red' : 'black' }]} />
-                    <Text style={[styles.tabTitle,{color: selectedTab == 0 ? 'red' : 'black'}]}>Delivery</Text>
+                    <Text style={[styles.tabTitle, { color: selectedTab == 0 ? 'red' : 'black' }]}>Delivery</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={[styles.tab,{borderTopWidth:1,borderTopColor:selectedTab == 1 ? 'red' : 'white'}]} onPress={() => { setSelectedTab(1) }}>
+                <TouchableOpacity style={[styles.tab, { borderTopWidth: 1, borderTopColor: selectedTab == 1 ? 'red' : 'white' }]} onPress={() => { setSelectedTab(1) }}>
                     <Image source={require('../images/dining.png')} style={[styles.tabIcon, { tintColor: selectedTab == 1 ? 'red' : 'black' }]} />
-                    <Text style={[styles.tabTitle,{color: selectedTab == 1 ? 'red' : 'black'}]}>Dining</Text>
+                    <Text style={[styles.tabTitle, { color: selectedTab == 1 ? 'red' : 'black' }]}>Dining</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={[styles.tab,{borderTopWidth:1,borderTopColor:selectedTab == 2 ? 'red' : 'white'}]} onPress={() => { setSelectedTab(2) }}>
+                <TouchableOpacity style={[styles.tab, { borderTopWidth: 1, borderTopColor: selectedTab == 2 ? 'red' : 'white' }]} onPress={() => { setSelectedTab(2) }}>
                     <Image source={require('../images/zomoland.png')} style={[styles.tabIcon, { tintColor: selectedTab == 2 ? 'red' : 'black' }]} />
-                    <Text style={[styles.tabTitle,{color: selectedTab == 2 ? 'red' : 'black'}]}>ZomoLand</Text>
+                    <Text style={[styles.tabTitle, { color: selectedTab == 2 ? 'red' : 'black' }]}>ZomoLand</Text>
                 </TouchableOpacity>
             </View>
         </View>
--- src/tabs/Tab1.js
+++ src/tabs/Tab1.js
@@ -1,12 +1,107 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 
-const tab1 = () => {
-  return (
-    <View>
-      <Text>tab1</Text>
-    </View>
-  )
+const Tab1 = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.headerLeft}>
+                    <Image source={require('../images/location.png')} style={styles.location} />
+                    <View>
+                        <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+                            <Text style={styles.locationBold}>Mangalore</Text>
+                        </View>
+                        <Text style={styles.locationSmall}>Kaikamba</Text>
+                    </View>
+
+                </View>
+
+                <View style={styles.headerRight}>
+                    <Image source={require('../images/LangIcon.png')} style={[styles.location, { marginRight: 20 }]} />
+                </View>
+            </View>
+            <View style={styles.searchBar}>
+                <Image source={require('../images/search.png')} style={styles.location} />
+                <Text style={styles.searchText}>Search Items</Text>
+                <Image source={require('../images/voice.png')} style={styles.location} />
+            </View>
+            <View>
+                <FlatList
+                    data = {[1,1,1,1,1,1,1]}
+                    renderItem={{item,inde} = () => {
+                        return(
+                            <TouchableOpacity style = {styles.filterItems}>
+                                <View>
+
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+            </View>
+        </View>
+    )
 }
 
-export default tab1
+export default Tab1
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    header: {
+        height: responsiveHeight(8),
+        width: responsiveWidth(100),
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    headerLeft: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    headerRight: {
+        flexDirection: 'row'
+    },
+    location: {
+        tintColor: 'red',
+        width: responsiveWidth(5),
+        height: responsiveWidth(5),
+        marginLeft: 5
+    },
+    locationBold: {
+        fontWeight: '800',
+        color: 'black'
+    },
+    locationSmall: {
+        marginLeft: 10,
+        color: 'black'
+    },
+    searchBar: {
+        height: responsiveHeight(6),
+        borderWidth: 0.2,
+        borderColor: "#8e8e8e",
+        alignSelf: 'center',
+        width: responsiveWidth(90),
+        borderRadius: 8,
+        marginTop: 20,
+        backgroundColor: "#f2f2f2",
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        justifyContent: 'space-evenly'
+    },
+    searchText: {
+        color: 'black',
+        width: '80%',
+        marginLeft: 20
+    },
+    filterItems:{
+        
+    }
+})
